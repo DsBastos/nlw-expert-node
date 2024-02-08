55f2e6a2973af31709b1a737bf841fadd6a76914
Daniel Bastos <estgv18113@alunos.estgv.ipv.pt>
2024-02-08 00:26:20 +0000

    aula 2

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,19 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
-import { z } from 'zod'
+import cookie from '@fastify/cookie'
+import { createPoll } from './routes/create-polls'
+import { getPoll } from './routes/get-polls'
+import { voteOnPoll } from './routes/vote-on-poll'
 
 const app = fastify()
+
+app.register(cookie, {
+  secret: 'polls-app-nlw',
+  hook: 'onRequest',
+})
+
+app.register(createPoll)
+app.register(getPoll)
+app.register(voteOnPoll)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP Server running on port 3333')
